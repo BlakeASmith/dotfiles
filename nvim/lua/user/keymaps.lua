1583417df19d7e1a497a4db5d0738656b30349e2
Blake Smith <blake@mac.lan>
2025-10-31 13:36:49 -0700

    feat: center on go-to-definition

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,13 +1,16 @@
 -- General navigation and visual mode keymaps
 
 -- Scroll with cursor centered
-vim.keymap.set('n', '<C-d>', '<C-d>zz', { desc = 'Scroll downwards' })
-vim.keymap.set('n', '<C-u>', '<C-u>zz', { desc = 'Scroll upwards' })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll downwards" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll upwards" })
 
 -- Search navigation with cursor centered
-vim.keymap.set('n', 'n', 'nzzzv', { desc = 'Next result' })
-vim.keymap.set('n', 'N', 'Nzzzv', { desc = 'Previous result' })
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next result" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous result" })
 
 -- Indent while remaining in visual mode
-vim.keymap.set('v', '<', '<gv')
-vim.keymap.set('v', '>', '>gv')
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
+-- Center after go to definitio
+vim.keymap.set("n", "gd", "gdzz")
